18fc0c74f5241b2c57c99f512869fe96dcb54430
Benadjaoud Abdelhak <89075329+hakouben@users.noreply.github.com>
2022-03-07 02:50:16 +0100

    input&feedconf

--- twitter-clone/components/Feed.js
+++ twitter-clone/components/Feed.js
@@ -1,7 +1,19 @@
+import { SparklesIcon } from "@heroicons/react/solid";
 import React from "react";
-
+import Input from "../components/Input"
 function Feed() {
-  return <div className=" text-white flex-grow  border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[380px]">Feed</div>;
+  return <div className=" flex-grow  border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[380px]"> 
+  <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50  bg-black border-b border-gray-700">
+    <h2 className=" text-lg  sm:text-xl  font-bold ">
+      Acceuil
+    </h2>
+    <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
+      <SparklesIcon/>
+    </div>
+    </div>
+    <Input/>
+  </div>
 }
+  
 
 export default Feed;
---
+++ twitter-clone/components/Input.js
@@ -0,0 +1,28 @@
+import { XIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+function Input() {
+    const[input,setInput]=useState("");
+    const[selectedFile,setSelectedFile] = useState(null);
+
+    return <div className={`border-b border-gray-700 p-4 flex space-x-3`}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsk7CNY1CFN_p0sfY6zR8KeobcLgl9Zs1xxw&usqp=CAU"className="h-11 w-11 rounded-full cursor-pointer"/>
+      <div className="w-full divide-y divide-gray-700">
+           
+            <div className={``}>
+                <textarea value={input}
+                 rows="2" 
+                 className=" bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]" 
+                onChange={(e)=> setInput(e.target.value)}
+                placeholder="Quoi de Neuf ?"/>
+            
+                <div className="relative">
+                <div className="absolute w-8 h-8 hover:bg-[#15181c] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer">
+                    <XIcon className=" text-white h-5"/>
+                    </div >
+                    <img src={selectedFile} className=" rounded-2xl max-h-80 object-contain"/>
+                    </div>
+                    </div>
+      </div>
+  </div>
+}
+export default Input;
