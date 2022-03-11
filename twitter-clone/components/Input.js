fdf5b0c8b284e8bc496c52ac92a172e3454fedec
Benadjaoud Abdelhak <89075329+hakouben@users.noreply.github.com>
2022-03-11 16:09:21 +0100

    Update Input.js

--- twitter-clone/components/Input.js
+++ twitter-clone/components/Input.js
@@ -13,7 +13,7 @@ function Input() {
                  rows="2" 
                  className=" bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]" 
                 onChange={(e)=> setInput(e.target.value)}
-                placeholder="Quoi de Neuf ?"/>
+                placeholder="Quoi de Neuf !?"/>
             
                 <div className="relative">
                 <div className="absolute w-8 h-8 hover:bg-[#15181c] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer">
